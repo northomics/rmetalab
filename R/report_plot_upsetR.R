92ce1fd584524fb069f20501c0882aaeddc1cfa6
Zhibin <9056380+ningzhibin@users.noreply.github.com>
2022-03-10 16:44:31 -0500

    initialize

--- R/report_plot_upsetR.R
+++ R/report_plot_upsetR.R
@@ -35,7 +35,7 @@ report_plot_upsetR <- function(df_intensity, meta_table = NULL){
       as.data.frame %>%
       rownames_to_column(var = "name")
     # plot
-    plot_experiment <- upset(df_intensity_for_upset,nsets = ncol(df_intensity), order.by = "freq", number.angles = 30, point.size = 4, line.size = 1,
+    plot_experiment <- UpSetR::upset(df_intensity_for_upset,nsets = ncol(df_intensity), order.by = "freq", number.angles = 30, point.size = 4, line.size = 1,
                              text.scale = c(1.5, 1.3, 1.5, 1.5, 1.5, 1.75), mb.ratio = c(0.6, 0.4))
 
     plot_list <- list( plot_experiment = plot_experiment)
